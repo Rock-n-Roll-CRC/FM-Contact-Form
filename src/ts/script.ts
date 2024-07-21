c705729ca78ba0e1f8b9f47d25c15482b5c0b375
Danil Dikhtyar <danil.dikhtyar@gmail.com>
2024-07-21 23:24:31 +0700

    🚧 Updated index.html, script.ts.

--- src/ts/script.ts
+++ src/ts/script.ts
@@ -41,7 +41,12 @@ radioFieldset?.addEventListener("click", (e: MouseEvent): void => {
 submitButton?.addEventListener("click", (e: MouseEvent): void => {
   e.preventDefault();
 
-  // Hiding all validation error messages
+  // Hiding all validation error messages and error styles
+  document
+    .querySelectorAll(".contact-form__input--invalidated")
+    .forEach((invalidatedInput: Element): void => {
+      invalidatedInput.classList.remove("contact-form__input--invalidated");
+    });
   document
     .querySelectorAll(".contact-form__input-validation-error-message")
     .forEach((message: Element): void => {
@@ -52,12 +57,15 @@ submitButton?.addEventListener("click", (e: MouseEvent): void => {
 
   // Checking for empty inputs
   inputs.forEach((input: HTMLInputElement): void => {
-    if (input.value === "")
+    if (input.value === "") {
       input.parentElement
         ?.querySelector(".contact-form__input-validation-error-message")
         ?.classList.add(
           "contact-form__input-validation-error-message--visible",
         );
+
+      input.classList.add("contact-form__input--invalidated");
+    }
   });
 
   // Validating email input field
@@ -65,10 +73,13 @@ submitButton?.addEventListener("click", (e: MouseEvent): void => {
     !emailInput?.value.match(
       /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/,
     )
-  )
+  ) {
     emailInput?.parentElement
       ?.querySelector(".contact-form__input-validation-error-message")
       ?.classList.add("contact-form__input-validation-error-message--visible");
+
+    emailInput?.classList.add("contact-form__input--invalidated");
+  }
 
   // Validating radio input field
   if (
